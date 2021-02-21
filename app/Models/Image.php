e658634183999239c189276813bc3c9593040847
Manuel <carlosd99m@gmail.com>
2021-02-20 21:08:43 -0600

    Relaciones de la bd a base de modelos

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model
 {
     use HasFactory;
+
+    // Relacion porlimorfica
+
+    public function imageable(){
+        return -$this->morphTo();
+    }
 }
